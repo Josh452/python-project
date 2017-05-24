df57560870c87405de4daab44e380913abf5e4dc
Josh <joshuactristram@gmail.com>
2017-05-25 10:55:04 +1200

    done

--- bot.py
+++ bot.py
@@ -1,4 +1,34 @@
 import sys
 import os
 from core import icore
-icore.start()
+
+def end():
+        exit = input("Do you want to exit? y or n \n")
+        no = "n" or "N"
+        yes = "y" or "N"
+        if exit==no:
+            print("-Info- Restarting")
+            ibot()
+
+        if exit==yes:
+            print ("-Info- Exiting")
+            sys.exit
+        else:
+            print("-Info- Not valid input")
+            end()
+
+def ibot():
+    icore.start()
+    icore.yn("are you haveing a good day?", "Thats good to hear", "Damn thats not too good. I am sure u will have a better one temmrow.")
+    icore.yn("Are you cool?", "Yep!!", "Nah your cool")
+    icore.type("What is your job.", "Thats cool, I have always wanted to a", "!")
+    icore.type("Who is your best friend?", "Wow! they sound cool! I like the name", " :)")
+    icore.type("When is your birthday?", "Lit! Always wanted my birthday to be in", "!")
+    icore.yn("Do you like memes?", "You are a lit person", "You sould try them!")
+    icore.yn("Do you like memes?", "You are a lit person", "You sould try them!")
+    icore.type("What school do you do to?", "Woah! ", ", What a lit school")
+    icore.type("are you a pc or xbox gamer?", "Meh.. I guess the are all ok.. but", " you can do better")
+    icore.yn("Are you in our class", "Woah you are cooooool", "Next time ;)")
+
+ibot()
+end()
--- core.py
+++ core.py
@@ -1,3 +1,5 @@
+
+
 #Josh's lit bot
 class icore():
     def start():
@@ -7,21 +9,27 @@ class icore():
         #Get name
         hated_names = ("aden")
         name = input("Hello, what is your name? \n")
+        print("Hello %s!" % name)
+        print("I like you already. Lets get to know you")
         if name==hated_names:
             print("Piss of :)")
+            sys.exit
 
-
-    def end():
-        exit = input("Do you want to exit? y or n \n")
+    def yn(q, yr, nr):
+        print("type y (yes) or n (no)")
+        ask = input(q)
         no = "n" or "N"
         yes = "y" or "N"
-        if exit==no:
-            print("-Info- Restarting")
-            icore.start()
-
-        if exit==yes:
-            print ("-Info- Exiting")
+        if ask==yes:
+            print (yr)
+            return
+        if ask==no:
+            print(nr)
+            return
         else:
-            print("-Info- Not valid input")
-            icore.end()
-            
+            print("invaid input. try again.")
+            icore.yn(q, yr, nr)
+
+    def type(q, a, af):
+        ak = input(q)
+        print(a + " " + ak + af)
